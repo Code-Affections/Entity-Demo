4055b5c3274a2bad1540d816305ae29851d6ec47
vishnukumarps <vishnukumar29518@gmail.com>
2020-10-27 22:45:59 +0530

    Add project files.

---
+++ ConsoleApp3/Model/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ConsoleApp3.Model
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string MobileNo { get; set; }
+
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿using ConsoleApp3.Model;
+using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StudentAppDbContext ob = new StudentAppDbContext();
+
+            Student s1 = new Student();
+            s1.Name = "Vishnu";
+        
+            s1.MobileNo = "898989898";
+
+
+
+            ob.Add(s1);//
+
+            ob.SaveChanges();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
